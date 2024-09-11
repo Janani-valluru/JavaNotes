512c7b20c2ab0694f35ed3a154217e38a23067f8
Janani V A <janani.valluru@gmail.com>
2024-09-11 21:54:14 +0530

    Add files via upload
    Assignment 2 (polymorphism) 
    Overriding 3

---
+++ EletricitybillOverriding.java
@@ -0,0 +1,59 @@
+
+
+class ElectricityBill{
+	void calculateBill() {
+		int unitsPermeter =180;
+		int fee =100;
+		int bill = fee*unitsPermeter;
+		System.out.println("the basic bill :"+ bill);
+		}
+		
+	}
+
+class ResidentialCustomer extends ElectricityBill{
+	void calculateBill() {
+		int unitsPermeter =180;
+		int fee =200;
+		int bill = fee*unitsPermeter;
+		System.out.println("the ResidentialCustomer bill :"+ bill);
+		}
+		
+	}
+
+class CommercialCustomer extends ElectricityBill{
+	void calculateBill() {
+		int unitsPermeter =180;
+		int fee =300;
+		int bill = fee*unitsPermeter;
+		System.out.println("the CommercialCustomer bill :"+ bill);
+		}
+		
+	}
+
+class IndustrialCustomer extends ElectricityBill{
+	void calculateBill() {
+		int unitsPermeter =180;
+		int fee =400;
+		int bill = fee*unitsPermeter;
+		System.out.println("the IndustrialCustomer bill :"+ bill);
+		}
+		
+	}
+
+public class EletricitybillOverriding {
+
+	public static void main(String[] args) {
+		ElectricityBill eb = new ElectricityBill();
+		eb.calculateBill();
+		
+		ResidentialCustomer rc = new ResidentialCustomer();
+		rc.calculateBill();
+		
+		CommercialCustomer cc = new CommercialCustomer();
+		cc.calculateBill();
+		
+		IndustrialCustomer ic = new IndustrialCustomer();
+		ic.calculateBill();
+	}
+
+}
